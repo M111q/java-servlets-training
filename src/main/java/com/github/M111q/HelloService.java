e1245900b283cfc37175016852d8e86f162f348c
M111q <miq1991@gmail.com>
2020-01-15 16:48:53 +0100

    add new test and fix lang string problem

--- src/main/java/com/github/M111q/HelloService.java
+++ src/main/java/com/github/M111q/HelloService.java
@@ -1,11 +1,16 @@
 package com.github.M111q;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Optional;
 
 class HelloService {
 
     static final String FALLBACK_NAME = "world";
     static final Lang FALLBACK_LANG = new Lang(1L, "Hello", "en");
+
+    private final Logger logger = LoggerFactory.getLogger(HelloService.class);
 
     private LangRepository repository;
 
@@ -18,7 +23,13 @@ class HelloService {
     }
 
     String prepareGreeting(String name, String lang) {
-        var langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
+        Long langId;
+        try {
+            langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
+        } catch (NumberFormatException e) {
+            logger.warn("wrong lang Id format (non numeric) " + lang);
+            langId = FALLBACK_LANG.getId();
+        }
         var welcomeWord = repository.findById(langId).orElse(FALLBACK_LANG).getWelcomeMsg();
         var nameToWelcome = Optional.ofNullable(name).orElse(FALLBACK_NAME);
         return welcomeWord + " " + nameToWelcome + "!";
--- src/test/java/com/github/M111q/HelloServiceTest.java
+++ src/test/java/com/github/M111q/HelloServiceTest.java
@@ -10,6 +10,8 @@ import static org.junit.Assert.*;
 public class HelloServiceTest {
 
     public static final String WELCOME_MSG = "Hello";
+    public static final String FALLBACK_ID_WELCOME = "Hola";
+
     private HelloService SUT;
 
 /*
@@ -46,43 +48,38 @@ public class HelloServiceTest {
     @Test
     public void test_prepareGreeting_ShouldReturnDefaultLangGreeting() {
         // given
-        var fallbackIdWelcome = "Hola";
-        var mockRepository = new LangRepository() {
-            @Override
-            Optional<Lang> findById(Long langId) {
-                if (langId.equals(HelloService.FALLBACK_LANG.getId())) {
-                    return Optional.of((new Lang(null, fallbackIdWelcome, null)));
-                }
-                return Optional.empty();
-            }
-        };
-
+        var mockRepository = fallbackLangIdRepository();
         var SUT = new HelloService(mockRepository);
         // when
         var result = SUT.prepareGreeting(null, null);
         // then
-        assertEquals(fallbackIdWelcome + " " + HelloService.FALLBACK_NAME + "!", result);
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
     }
 
-//    @Test
-//    public void test_prepareGreeting_ShouldReturnGreetingWithGivenName() {
-//
-//        var name = "test";
-//
-//        var result = SUT.prepareGreeting(name , null);
-//
-//        assertEquals("Hello " + name + "!", result);
-//    }
-//
-//    @Test
-//    public void test_prepareGreeting_ShouldReturnGreetingWithBlank() {
-//
-//        var name = "";
-//
-//        var result = SUT.prepareGreeting(name, null);
-//
-//        assertEquals("Hello " + name + "!", result);
-//    }
+    @Test
+    public void test_prepareGreeting_ShouldReturnDefaultLangGreeting_textLang() {
+        // given
+        String wrongLangId = "test";
+        var mockRepository = fallbackLangIdRepository();
+        var SUT = new HelloService(mockRepository);
+        // when
+        var result = SUT.prepareGreeting(null, wrongLangId);
+        // then
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    private LangRepository fallbackLangIdRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Long langId) {
+                if (langId.equals(HelloService.FALLBACK_LANG.getId())) {
+                    return Optional.of((new Lang(null, FALLBACK_ID_WELCOME, null)));
+                }
+                return Optional.empty();
+            }
+        };
+    }
+
 
     private LangRepository alwaysReturningHelloLangRepository() {
         return new LangRepository() {
