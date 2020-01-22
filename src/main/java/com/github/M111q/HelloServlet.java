5120a9c8f23fad3a4310c8d953bc66bb1376141f
M111q <miq1991@gmail.com>
2020-01-22 10:58:39 +0100

    jackson and LangServlet added

--- src/main/java/com/github/M111q/HelloServlet.java
+++ src/main/java/com/github/M111q/HelloServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Hello", urlPatterns = {"/api/*"})
+@WebServlet(name = "Hello", urlPatterns = {"/api"})
 public class HelloServlet extends HttpServlet {
     private static final String REQ_PARAM_NAME = "name";
     private static final String REQ_PARAM_LANG = "lang";
--- src/main/java/com/github/M111q/LangRepository.java
+++ src/main/java/com/github/M111q/LangRepository.java
@@ -6,9 +6,22 @@ import java.util.Optional;
 
 public class LangRepository {
 
+    List<Lang> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        var result = session.createQuery("from Lang", Lang.class).list();
+        transaction.commit();
+        session.close();
+
+        return result;
+    }
+
+//todo refactor
+
     Optional<Lang> findById(Integer langId) {
         var session = HibernateUtil.getSessionFactory().openSession();
         var transaction = session.beginTransaction();
+
         var result = session.get(Lang.class, langId);
         transaction.commit();
         session.close();
