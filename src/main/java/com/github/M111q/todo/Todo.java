9b9d4b56ab34586dd74391b7e322f0fb5a0e8a87
M111q <miq1991@gmail.com>
2020-01-29 18:49:49 +0100

    added todo model class

---
+++ src/main/java/com/github/M111q/todo/Todo.java
@@ -0,0 +1,53 @@
+package com.github.M111q.todo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+class Todo {
+
+    @Id
+    @GeneratedValue(generator="inc")
+    @GenericGenerator(name="inc", strategy = "increment")
+    private int id;
+    private String text;
+    private boolean done;
+
+    public Todo() {
+    }
+
+    public Todo(int id, String text, boolean done) {
+        this.id = id;
+        this.text = text;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
